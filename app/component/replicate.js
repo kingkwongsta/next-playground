c516485295cc0a117919c6e77fc6ee23ad51c53f
Kwongster <bryant.j.kwong@gmail.com>
2024-01-28 23:02:37 +0000

    update secrets and width

--- app/component/replicate.js
+++ app/component/replicate.js
@@ -51,8 +51,8 @@ export default function Replicate() {
         <a href="https://replicate.com/stability-ai/stable-diffusion">SDXL</a>:
       </p>
       <form className="space-x-8" onSubmit={handleSubmit}>
-        <input
-          className="border-slate-800 border-2 min-h-[200px] min-w-[200px] p-4"
+        <textarea
+          className="border-slate-800 border-2 min-h-[200px] min-w-[500px] p-4 break-words"
           type="text"
           name="prompt"
           placeholder="Enter a prompt to display an image"
